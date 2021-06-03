b15ff4eb44e5b8abb04e8b3359e74343f7654d1b
Davi Freire <davifreireangelim@gmail.com>
2021-06-02 21:55:09 -0300

    2/6

--- crud/delete.php
+++ crud/delete.php
@@ -1,33 +1,25 @@
 <?php
-
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$senha = $_POST['senha'];
-
-var_dump($nome,$email,$senha)
-
-
 //Dados de conexão LOCAL
 $servername = "localhost";
 $username   = "root";
 $password   = "";
 $dbname     ="blog";
 
-//Dados de conexão INFINITYFREE
-$servername = "sql207.epizy.com"
-$username = "epiz_28557363"
-$password = "dUh8GAR5PUt"
-$dbname = "epiz_28557363_blog"
+//Dados de conexão HEROKU
+
+
 //-------
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO users (name, email, password)
-  VALUES ('$nome', '$email', '$senha')";
+
+  // sql to delete a record
+  $sql = "DELETE FROM users WHERE id=7";
+
   // use exec() because no results are returned
   $conn->exec($sql);
-  echo "Usuário cadastrado com sucesso!";
+  echo "Informação deletada com sucesso";
 } catch(PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
--- index.php
+++ index.php
@@ -1,3 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
-echo "Praticando conexão com Banco de Dados MySQL";
+    echo "Praticando conexão com Banco de Dados MySQL";
+     ?>
+     <h1> Back End </h1>
+     <a href="crud/create.php">Create</a>
+     <a href="crud/read.php">Read</a>
+     <a href="crud/update.php">Update</a>
+     <a href="crud/delete.php">Delete</a>
+       
+       <h1> Front End </h1> 
+     <a href="cad_users.html">Cadastrar Usuário</a>
+
+
+
+</body>
+</html>
+
